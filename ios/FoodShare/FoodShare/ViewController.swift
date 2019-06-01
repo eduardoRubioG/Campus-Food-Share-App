b3e62d755618111467d2a0bdec4d7fc767482125
Haider Khan <hkhan@datalofts.com>
2019-05-31 19:27:25 -0700

    Initialized Android and iOS projects

---
+++ ios/FoodShare/FoodShare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodShare
+//
+//  Created by Haider Khan on 5/31/19.
+//  Copyright © 2019 campusfoodshare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
